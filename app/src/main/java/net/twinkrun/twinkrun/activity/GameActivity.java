729baaa4a16e99c894e1dd684ba7d8eba3eb437e
kvvzr <kwzr@twinkrun.net>
2015-10-22 11:49:42 +0900

    game model

--- app/src/main/java/net/twinkrun/twinkrun/activity/GameActivity.java
+++ app/src/main/java/net/twinkrun/twinkrun/activity/GameActivity.java
@@ -2,17 +2,23 @@ package net.twinkrun.twinkrun.activity;
 
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import net.twinkrun.twinkrun.R;
+import net.twinkrun.twinkrun.model.Game;
+import net.twinkrun.twinkrun.model.IGame;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class GameActivity extends BaseBleActivity {
+public class GameActivity extends BaseBleActivity implements IGame {
+
+    private Game mGame;
 
     @OnClick(R.id.play_button)
     public void onClickPlayButton() {
+        mGame.togglePlayState();
     }
 
     @Override
@@ -26,6 +32,7 @@ public class GameActivity extends BaseBleActivity {
             getSupportActionBar().hide();
         }
 
+        mGame = new Game(this);
         setupBle("kwzr,25", getResources().getString(R.string.advertise_uuid), mScanCallback);
     }
 
@@ -33,6 +40,37 @@ public class GameActivity extends BaseBleActivity {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
             super.onScanResult(callbackType, result);
+            mGame.addScanResult(result);
         }
     };
+
+    @Override
+    public void onUpdateCountDown(int count) {
+
+    }
+
+    @Override
+    public void onStartGame() {
+
+    }
+
+    @Override
+    public void onUpdateScore(int score) {
+
+    }
+
+    @Override
+    public void onFlash(Color color) {
+
+    }
+
+    @Override
+    public void onUpdateRole(Color color, int score) {
+
+    }
+
+    @Override
+    public void onEndGame() {
+
+    }
 }
